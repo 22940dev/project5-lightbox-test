65d608ce0be57726cbac84226777fc6f3035d2a6
spsteady <spsteady@gmail.com>
2021-03-02 10:33:40 -0600

    review

--- js/app.js
+++ js/app.js
@@ -1,1 +1,22 @@
 baguetteBox.run('.gallery');
+// const search = new Filter('search', 'data-caption');
+
+/****************************************/
+/* SEARCH FUNCTION                      */
+/****************************************/
+
+
+function photoSearch() { 
+    let input = document.getElementById('search').value;
+    let a = document.getElementsByTagName('a');
+    input = input.toLowerCase(); 
+     
+    
+    for (i = 0; i < a.length; i++) { 
+        if (!a[i].getAttribute('data-caption').toLowerCase().includes(input)) { 
+            a[i].style.display="none"; 
+        } else { 
+            a[i].style.display="";				 
+            } 
+     } 
+} 
